95bed65976aae9315d2c296e38359ecd9f468436
Karen <karenperarnau@gmail.com>
2023-02-16 19:01:35 -0430

    Adding css, levels of difficulty and play again button event

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-const container = document.getElementById("container-cards");
-const buttonPlayAgain = document.getElementById("play-again");
-//buttonPlayAgain.style.display = "none";
+document.getElementById('difficulty').addEventListener('change', selectDifficulty)
 
+const container = document.getElementById("container-cards");
+let difficulty = {easy: 25, medium: 36, hard: 64}
+let difficultySelected = 36
 let values = [];
 
+function selectDifficulty(e){
+   difficultySelected = difficulty[e.currentTarget.value]
+   values = []
+   container.innerHTML = ''
+   loadCards()
+}
+
 function randomCardsValues() {
-  while (values.length < 36) {
-    let randomNum = Math.floor(Math.random() * (18 - 1 + 1)) + 1;
+
+  while (values.length < difficultySelected) {
+    let randomNum = Math.floor(Math.random() * ((difficultySelected/2) - 1 + 1)) + 1;
     let index = values.indexOf(randomNum);
 
     if (index === -1 || values.indexOf(randomNum, index + 1) === -1) {
@@ -16,9 +25,24 @@ function randomCardsValues() {
 }
 
 function loadCards() {
-  randomCardsValues();
+  
+  randomCardsValues()
+  const grid = Math.sqrt(values.length)
+
+  if(grid === 5){
+container.style.gridTemplateColumns = `repeat(${grid}, 18%)`
+container.style.gridTemplateRows = `repeat(${grid}, 18%)`
+  } else  if(grid === 6 ){
+    container.style.gridTemplateColumns = `repeat(${grid}, 15%)`
+    container.style.gridTemplateRows = `repeat(${grid}, 15%)`
+      }
+      else {
+    container.style.gridTemplateColumns = `repeat(${grid}, 11%)`
+    container.style.gridTemplateRows = `repeat(${grid}, 11%)`
+  }
+
   let i = 0;
-  while (i < 36) {
+  while (i < values.length) {
     container.innerHTML += `
       <button id='card-${i}'>
         <div class='front'></div>
@@ -32,6 +56,7 @@ function loadCards() {
   cardsEvent((card) => {
     card.addEventListener("mouseover", onMouseOver);
     card.addEventListener("mouseout", onMouseOut);
+ 
     card.addEventListener("click", onClickCard);
   });
 }
@@ -49,10 +74,10 @@ function onClickCard(event) {
       previousCardRevealed.id !== currCardRevealed.id
     ) {
       setTimeout(() => {
-       availablesCards(currCardRevealed.innerText)
         currCardRevealed.classList.add("found");
         previousCardRevealed.classList.add("found");
         previousCardRevealed = null;
+        availablesCards(currCardRevealed.innerText)
         changeStateCards();
       }, 1000);
     } else {
@@ -69,14 +94,18 @@ function onClickCard(event) {
 }
 
 function availablesCards(card){
+const buttonPlayAgain = document.getElementById("play-again");
+
  let firstCard = values.indexOf(card)
  let secondCard = values.indexOf(card, firstCard+1)
 
  values.splice(0)
  //values.splice(firstCard,1)
-// values.splice(secondCard-1,1)
+ //values.splice(secondCard-1,1)
+ 
  if(values.length === 0){
-  buttonPlayAgain.style.color = 'white'
+  buttonPlayAgain.addEventListener('click', playAgain)
+  buttonPlayAgain.classList.add('visible')
  }
 }
 
@@ -88,6 +117,12 @@ function onMouseOver(event) {
 function onMouseOut(event) {
   const card = event.currentTarget;
   card.classList.remove("hover");
+}
+//font-family: 'Signika Negative', sans-serif;
+function playAgain(){
+  document.getElementById("play-again").classList.remove('visible')
+  container.innerHTML = ''
+  loadCards()
 }
 
 function changeStateCards() {
@@ -114,4 +149,7 @@ function cardsEvent(fn) {
   });
 }
 
-window.addEventListener('load', loadCards)
+window.addEventListener('load',() => {
+  randomCardsValues();
+  loadCards()
+} )
