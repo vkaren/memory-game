5aa1ea461d0cd6d7f9e583be136d9bb28dad4e25
Karen <karenperarnau@gmail.com>
2023-02-20 14:41:53 -0430

    Optimizing code

--- js/script.js
+++ js/script.js
@@ -1,62 +1,59 @@
-document.getElementById('difficulty').addEventListener('change', selectDifficulty)
+document
+  .getElementById("difficulty")
+  .addEventListener("change", selectDifficulty);
 
 const container = document.getElementById("container-cards");
-let difficulty = {easy: 25, medium: 36, hard: 64}
-let difficultySelected = 36
-let values = [];
+const playAgainButton = document.getElementById("play-again");
+playAgainButton.addEventListener("click", playAgain);
+const difficulty = { easy: 16, medium: 36, hard: 64 };
+let difficultySelected = difficulty.medium;
+let cardValues = [];
 
-function selectDifficulty(e){
-   difficultySelected = difficulty[e.currentTarget.value]
-   values = []
-   container.innerHTML = ''
-   loadCards()
+function selectDifficulty(e) {
+  difficultySelected = difficulty[e.currentTarget.value];
+  playAgainButton.classList.remove("visible");
+  container.innerHTML = "";
+  cardValues = [];
+  loadCards();
 }
 
 function randomCardsValues() {
+  while (cardValues.length < difficultySelected) {
+    let randomNum =
+      Math.floor(Math.random() * (difficultySelected / 2 - 1 + 1)) + 1;
+    let index = cardValues.indexOf(randomNum);
 
-  while (values.length < difficultySelected) {
-    let randomNum = Math.floor(Math.random() * ((difficultySelected/2) - 1 + 1)) + 1;
-    let index = values.indexOf(randomNum);
-
-    if (index === -1 || values.indexOf(randomNum, index + 1) === -1) {
-      values.push(randomNum);
+    if (index === -1 || cardValues.indexOf(randomNum, index + 1) === -1) {
+      cardValues.push(randomNum);
     }
   }
 }
 
 function loadCards() {
-  
-  randomCardsValues()
-  const grid = Math.sqrt(values.length)
+  randomCardsValues();
 
-  if(grid === 5){
-container.style.gridTemplateColumns = `repeat(${grid}, 18%)`
-container.style.gridTemplateRows = `repeat(${grid}, 18%)`
-  } else  if(grid === 6 ){
-    container.style.gridTemplateColumns = `repeat(${grid}, 15%)`
-    container.style.gridTemplateRows = `repeat(${grid}, 15%)`
-      }
-      else {
-    container.style.gridTemplateColumns = `repeat(${grid}, 11%)`
-    container.style.gridTemplateRows = `repeat(${grid}, 11%)`
-  }
+  const grid = Math.sqrt(difficultySelected);
+  const gridTemplate = `repeat(${grid}, ${Math.floor(100 / grid - 1)}%)`;
+
+  container.style.gridTemplateColumns = gridTemplate;
+  container.style.gridTemplateRows = gridTemplate;
 
   let i = 0;
-  while (i < values.length) {
+  while (i < cardValues.length) {
     container.innerHTML += `
       <button id='card-${i}'>
         <div class='front'></div>
         <div class='back'>
-          ${values[i]}
+          ${cardValues[i]}
         </div>
       </button>
     `;
     i++;
   }
+
   cardsEvent((card) => {
     card.addEventListener("mouseover", onMouseOver);
     card.addEventListener("mouseout", onMouseOut);
- 
     card.addEventListener("click", onClickCard);
   });
 }
@@ -68,7 +65,7 @@ function onClickCard(event) {
 
   if (previousCardRevealed) {
     changeStateCards();
-  
+
     if (
       previousCardRevealed.innerText === currCardRevealed.innerText &&
       previousCardRevealed.id !== currCardRevealed.id
@@ -77,8 +74,8 @@ function onClickCard(event) {
         currCardRevealed.classList.add("found");
         previousCardRevealed.classList.add("found");
         previousCardRevealed = null;
-        availablesCards(currCardRevealed.innerText)
         changeStateCards();
+        availablesCards(currCardRevealed.innerText);
       }, 1000);
     } else {
       setTimeout(() => {
@@ -93,63 +90,51 @@ function onClickCard(event) {
   }
 }
 
-function availablesCards(card){
-const buttonPlayAgain = document.getElementById("play-again");
+function availablesCards(card) {
+  // const buttonPlayAgain = document.getElementById("play-again");
+  let firstCard = cardValues.indexOf(card);
+  let secondCard = cardValues.indexOf(card, firstCard + 1);
 
- let firstCard = values.indexOf(card)
- let secondCard = values.indexOf(card, firstCard+1)
+  cardValues.splice(firstCard, 1);
+  cardValues.splice(secondCard - 1, 1);
 
- values.splice(0)
- //values.splice(firstCard,1)
- //values.splice(secondCard-1,1)
- 
- if(values.length === 0){
-  buttonPlayAgain.addEventListener('click', playAgain)
-  buttonPlayAgain.classList.add('visible')
- }
+  if (cardValues.length === 0) {
+    setTimeout(() => {
+      playAgainButton.classList.add("visible");
+    }, 1000);
+  }
 }
 
-function onMouseOver(event) {
-  const card = event.currentTarget;
-  card.classList.add("hover");
-}
-
-function onMouseOut(event) {
-  const card = event.currentTarget;
-  card.classList.remove("hover");
-}
-//font-family: 'Signika Negative', sans-serif;
-function playAgain(){
-  document.getElementById("play-again").classList.remove('visible')
-  container.innerHTML = ''
-  loadCards()
+function playAgain() {
+  playAgainButton.classList.remove("visible");
+  container.innerHTML = "";
+  loadCards();
 }
 
 function changeStateCards() {
   cardsEvent((card) => {
     card.disabled = !card.disabled;
     if (card.disabled) {
-        card.classList.remove("hover");
-        card.removeEventListener("mouseover", onMouseOver);
-        card.removeEventListener("mouseout", onMouseOut);
-  
+      card.classList.remove("hover");
+      card.removeEventListener("mouseover", onMouseOver);
+      card.removeEventListener("mouseout", onMouseOut);
     } else {
-        card.addEventListener("mouseover", onMouseOver);
-        card.addEventListener("mouseout", onMouseOut);
-
+      card.addEventListener("mouseover", onMouseOver);
+      card.addEventListener("mouseout", onMouseOut);
     }
   });
 }
+
+function onMouseOver(event) {
+  const card = event.currentTarget;
+  card.classList.add("hover");
+}
+function onMouseOut(event) {
+  const card = event.currentTarget;
+  card.classList.remove("hover");
+}
 function cardsEvent(fn) {
-  Object.values(container.children).forEach((card)=> {
-   if(card.id !== 'play-again' ) {
-     fn(card)
-   }
-   
-  });
+  Object.values(container.children).forEach((card) => fn(card));
 }
 
-window.addEventListener('load',() => {
-  randomCardsValues();
-  loadCards()
-} )
+window.addEventListener("load", () => loadCards());
